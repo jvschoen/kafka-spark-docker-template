fd734ed96cc944b72c99fdb21f8c09cc2a46d846
jvschoen <jvshoen@live.com>
2020-12-28 01:06:49 -0500

    set up docker compose with kafka and pyspark to send and receive messages. working on the restful predictions section

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Testing kafka spark streaming
 Based off:
 https://github.com/BogdanCojocar/medium-articles/tree/master/realtime_kafka
+
+And 
+[This](https://docs.docker.com/compose/gettingstarted/) For Redis for Flask caching
---
+++ sentiment/app.py
@@ -0,0 +1,42 @@
+import time
+
+from flask import Flask, jsonify, request
+import redis
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6397)
+sid = SentimentIntensityAnalyzer()
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    """This Creates a POST request with msg containing
+    message in form:
+    {"data": "some text to analyze"}
+
+    We use the SentimentIntensityAnalyzer 
+    to output a polarity score dictionary
+    as the response in the form of JSON
+
+    Returns:
+        str: JSON string of polarity score 
+    """
+    result = sid.polarity_scores(request.get_json()['data'])
+    num_preds = get_hit_count()
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(port=9000)
+    
